b1cc78dbf72c0b3dc68446fbf81bd799ce0f242c
Yevhen Bondar <eugenij@apexive.com>
2022-11-30 16:32:57 +0200

    added django-import-export for admin models

--- df_notifications/admin.py
+++ df_notifications/admin.py
@@ -4,10 +4,33 @@ from .models import UserDevice
 from django.contrib import admin
 from fcm_django.admin import DeviceAdmin
 from fcm_django.models import FCMDevice
+from import_export import resources
+from import_export.admin import ImportExportMixin
+
+
+class NotificationHistoryResourceMixin(resources.ModelResource):
+    class Meta:
+        import_id_fields = ("id",)
+        fields = (
+            # "id",
+            "users",
+            "channel",
+            "subject",
+            "body",
+            "body_html",
+            "data",
+            "timestamp",
+        )
+        skip_unchanged = True
+        report_skipped = True
+        export_order = fields
+        model = NotificationHistory
 
 
 @admin.register(NotificationHistory)
-class NotificationHistoryAdmin(admin.ModelAdmin):
+class NotificationHistoryAdmin(ImportExportMixin, admin.ModelAdmin):
+    resource_class = NotificationHistoryResourceMixin
+
     def get_users(self, obj):
         return ",".join(obj.users.values_list("email", flat=True))
 
@@ -31,7 +54,25 @@ class UserDeviceAdmin(DeviceAdmin):
     pass
 
 
+class NotificationTemplateResourceMixin(resources.ModelResource):
+    class Meta:
+        import_id_fields = ("id",)
+        fields = (
+            "id",
+            "channel",
+            "subject",
+            "body",
+            "body_html",
+            "data",
+        )
+        skip_unchanged = True
+        report_skipped = True
+        export_order = fields
+        model = NotificationTemplate
+
+
 @admin.register(NotificationTemplate)
-class NotificationTemplateAdmin(admin.ModelAdmin):
+class NotificationTemplateAdmin(ImportExportMixin, admin.ModelAdmin):
+    resource_class = NotificationTemplateResourceMixin
     search_fields = ("slug",)
     list_display = ("slug", "channel", "subject")
